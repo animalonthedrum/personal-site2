c3ec3d876c803dd829c8e09b182a6a802d8a5fda
Brett Miller <miller.brett.andrew@gmail.com>
2017-09-23 20:28:22 -0500

    playing with color

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,6 +6,45 @@ $(document).ready(function() {
 
 
   });
+  // var camera, scene, renderer;
+  // var geometry, material, mesh;
+  //
+  // init();
+  // animate();
+  //
+  // function init() {
+  //
+  //   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  //   camera.position.z = 1;
+  //
+  //   scene = new THREE.Scene();
+  //
+  //   geometry = new THREE.SphereGeometry(0.6, 0.3, 1);
+  //   material = new THREE.MeshNormalMaterial();
+  //
+  //   mesh = new THREE.Mesh(geometry, material);
+  //   scene.add(mesh);
+  //
+  //   renderer = new THREE.WebGLRenderer({
+  //     antialias: true
+  //   });
+  //   renderer.setSize(window.innerWidth, window.innerHeight);
+  //   document.body.appendChild(renderer.domElement);
+  //
+  // }
+  //
+  // function animate() {
+  //
+  //   requestAnimationFrame(animate);
+  //
+  //   mesh.rotation.x += 0.01;
+  //   mesh.rotation.y += 0.02;
+  //
+  //   renderer.render(scene, camera);
+  //
+  // }
+
+
 }); //end on ready
 
 
